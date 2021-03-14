013e9d05e90a28a4258c573d0410e9bd81d2f349
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-03-14 17:17:00 +0200

    Created Models - Basic models created but they are, of course, subject to change depending on future needs. - Note: Not sure if 'subject' of Profile should be just a field or a foreign key.

--- gubookhub_project/gubookhub_app/models.py
+++ gubookhub_project/gubookhub_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    url = models.URLField(max_length=256, blank=True)
+    favorite_count = models.IntegerField(default=0)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Favorite(models.Model):
+    book = models.ForeignKey(Book)
+    user = models.ForeignKey(Profile)
+
+    def __str__(self):
+        # User favorited: Book
+        return self.book + ' favorited by: ' + self.user.name 
+    
+
+class Course(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+    level = models.IntegerField()
+    subject = models.ForeignKey(Subject)
+
+    def __str__(self):
+        return self.title
+
+class Subject(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    level = models.IntegerField()
+    subject = models.CharField(max_length=128, blank=False) # Considering making it a foreign key later on
+
+    # @receiver(post_save, sender=User)
+    # def create_user_profile(sender, instance, created, **kwargs):
+    #     if created:
+    #         Profile.objects.create(user=instance)
+
+    # @receiver(post_save, sender=User)
+    # def save_user_profile(sender, instance, **kwargs):
+    #     instance.profile.save()
+    # https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
