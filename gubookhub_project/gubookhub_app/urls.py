09b17707b02f9268884820c8b790ce23035bdaaa
Zoltan Sojtory <2442602s@student.gla.ac.uk>
2021-03-14 23:35:09 +0100

    project urls

---
+++ gubookhub_project/gubookhub_app/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path
+from gubookhub_app import views
--- gubookhub_project/gubookhub_project/urls.py
+++ gubookhub_project/gubookhub_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from gubookhub_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+	path('', views.index, name='index'),
+	path('gubookhub_app/', include('gubookhub_app.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
