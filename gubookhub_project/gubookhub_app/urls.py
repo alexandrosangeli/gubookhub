35dc66538bbdf0c9927155f53ebb484549224723
Zoltan Sojtory <2442602s@student.gla.ac.uk>
2021-03-15 17:35:13 +0100

    urls

--- gubookhub_project/gubookhub_app/urls.py
+++ gubookhub_project/gubookhub_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from gubookhub_app import views
 
-app_name='rango'
+app_name='gubookhub_app'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('about/', views.about, name = 'about'),
+	path('register/', views.register, name='register'), 
+	path('login/', views.user_login, name='login'),
+	path('favourites/', views.favourites, name='favourites'),
+	path('search/', views.search, name='search'),
+	path('add_book/', views.add_book, name='add_book'),
+	path('<slug:subject_name_slug>/', views.subjects, name='subjects'),
+	path('<slug:subject_name_slug>/<slug:course_name_slug>/', views.courses, name='courses'),
+	path('<slug:subject_name_slug>/<slug:course_name_slug>/<slug:book_name_slug>/', views.books, name='books'),
 ]
