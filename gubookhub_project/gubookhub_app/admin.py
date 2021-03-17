c67b8b3fd842117bac12a657bbaaa1f1c0792e86
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2021-03-17 16:59:37 +0000

    configured admin page, created superuser, updated Profile model, created media directory

--- gubookhub_project/gubookhub_app/admin.py
+++ gubookhub_project/gubookhub_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from gubookhub_app.models import Profile
+from gubookhub_app.models import Subject, Course, Book
 
 # Register your models here.
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(Book)
+admin.site.register(Profile)
