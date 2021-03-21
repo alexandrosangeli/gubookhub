b20cc2146ec93c622a9595eed605346569b280c3
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-03-21 21:11:27 +0200

    Added minimal search functionality - Users can only retrieve a limited amount of books - Users can only search for a title - Response only includes title and a list of authors

---
+++ gubookhub_project/gubookhub_app/google_books_api.py
@@ -0,0 +1,14 @@
+import requests
+import json
+#from pprint import pprint
+
+def run_query(query):
+    
+    url = "https://www.googleapis.com/books/v1/volumes?q=" + query.strip()
+    response = requests.get(url).json()
+    print(type(response))
+    #pprint(response['items'])
+    return response['items']
+
+if __name__ == '__main__':
+    run_query("algorithms")
--- gubookhub_project/gubookhub_app/views.py
+++ gubookhub_project/gubookhub_app/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from gubookhub_app.google_books_api import run_query
 
 # Create your views here.
 
@@ -21,3 +22,16 @@ def about(request):
     context_dict['aboutmessage'] = 'This is a student made resource hub for the University of Glasgow students. It features a collection of useful textbooks and resources submitted by fellow students across various schools from various years.'
 
     return render(request, 'gubookhub/about.html', context = context_dict)
+
+def search(request):
+    results = []
+
+    if request.method == "POST":
+        query = request.POST["query"].strip()
+
+        if query:
+            api_response = run_query(query)
+            for item in api_response:
+                results.append(item['volumeInfo'])
+    
+    return render(request, 'gubookhub/search.html', context={'results':results})
