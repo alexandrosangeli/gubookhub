d15a00b3378f6136ee7e5d2bf171925d0a8ddc52
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2021-03-25 13:41:56 +0000

    Added CSSfor login.html, about.html

--- gubookhub_project/gubookhub_app/templatetags/template_tags.py
+++ gubookhub_project/gubookhub_app/templatetags/template_tags.py
@@ -4,5 +4,5 @@ from gubookhub_app.models import Course
 register = template.Library()
 
 @register.inclusion_tag('gubookhub/courses.html')
-def get_courses_list():
-    return {'courses': Course.objects.all()}
+def get_courses_list(num):
+    return {'courses': Course.objects.order_by('level')[:num]}
